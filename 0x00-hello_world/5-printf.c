7af737adbe9451b1f49aec9ded2c4b150b7cc13e
Dragonixj <delafiado@gmail.com>
2023-02-16 10:55:41 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Printf: prints the code in the ""
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	printf("%s", "with a proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
