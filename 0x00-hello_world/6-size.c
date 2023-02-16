603a00c42eaa02e0f8d854ef77b6a198b587aaa2
Dragonixj <delafiado@gmail.com>
2023-02-16 11:59:43 -0800

    commit added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,12 @@
  */
 int main(void)
 {
+	char z;
+	int x;
+	long int y;
+	long long int l;
+	float f;
+
 	printf("Size of a char:%lu  byte(s)\n", sizeof(z));
 	printf("Size of an int: %lu byte(s)\n", sizeof(x));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(y));
