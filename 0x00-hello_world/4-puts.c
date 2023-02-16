5bb575a94ae933588eb73ccbc707b0238f73c526
Dragonixj <delafiado@gmail.com>
2023-02-16 10:36:01 -0800

    first code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main -Is the entry point of the code.
+ *
+ * Return is always set to the value of zero (0) 
+ */
+int main(void)
+
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+
+}
+
+
+
+
