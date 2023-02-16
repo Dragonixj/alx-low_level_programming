b9beb40d61b6614c8201b8917ad478a013be4d5e
Dragonixj <delafiado@gmail.com>
2023-02-16 12:25:39 -0800

    commit added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main -Entry point
+*	 prints "and that .....useful" -Dora Korpar, 2015-10-19
+*		followed by a new line, to the standard error.
+*/
+int main(void)
+{	write(2,"and that piece of artis useful\" - Dora Korpar, 2015-10-19\n",
+		59);
+
+	return (1);
+}
