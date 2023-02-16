48bae5711fa68c7a699c2ac6811dd06653371020
Dragonixj <delafiado@gmail.com>
2023-02-16 12:29:49 -0800

    commit added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  *main -Entry point
@@ -7,7 +8,7 @@
 *		followed by a new line, to the standard error.
 */
 int main(void)
-{	write(2,"and that piece of artis useful\" - Dora Korpar, 2015-10-19\n",
+{	write(2, "and that piece of artis useful\" - Dora Korpar, 2015-10-19\n",
 		59);
 
 	return (1);
