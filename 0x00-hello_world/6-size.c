23e2f0aa94b1f2f75705f1ca9dad5f24390a48cf
Dragonixj <delafiado@gmail.com>
2023-02-16 11:37:30 -0800

    types size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,12 +13,14 @@ int main(void)
 	char z;
 	float f;
 	double d;
+	long long int l;
 
 	printf("Size of an int:%lu  byte(s)\n", sizeof(x));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(y));
 	printf("Size of a char: %lu byte(s)\n", sizeof(z));
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	printf("Size of a double: %lu byte(s)\n", sizeof(d));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(l));
 	return (0);
 
 }
