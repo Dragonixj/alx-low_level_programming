8fa7706334685f84037c8322b9c7eaeb86d3825a
Dragonixj <89875812+Dragonixj@users.noreply.github.com>
2023-02-17 21:43:52 +0000

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,10 +3,10 @@
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
  * followed by a new line, using putchar
- * Return: Always 0 (Success)
+ * Return: (0) Success
  */
 int main(void)
-{
+{ 20.04, with vim, emacs, curl,
 	int n;
 
 	for (n = 48; n < 58; n++)
