0dfc04a2f2c0fd396956a622752d210599aa4cec
Dragonixj <delafiado@gmail.com>
2023-02-21 02:00:55 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main -  prints putchar, followed by new line
+ * Return: (0) succes
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
